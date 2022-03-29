a09321389d06c05a0d49129df9f647852354128a
Luan Marco <rbblackslade@gmail.com>
2022-03-28 22:51:27 -0300

    a

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,7 +1,7 @@
 const contractAddress = "0xa1018f74b1aB3292742bD9dCFEf0ED1BAe32b0C7"; //Replace with your own contract address
 const chain = 'polygon'; // rinkeby or polygon
 
-const welcome_h1 = "Connect to METAMASK to get started";
+const welcome_h1 = "Connect to MetaMask to get started";
 const welcome_h2 = "";
 const welcome_p = "";
 const h1_presale_coming_soon = "Sloggers Drop Coming Soon";
