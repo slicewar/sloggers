2462f3523468db009acc660f1cfb53819ffeb073
Luan Marco <rbblackslade@gmail.com>
2022-04-02 19:52:05 -0300

    a

--- backend/src/config.js
+++ backend/src/config.js
@@ -75,7 +75,7 @@ let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually inclu
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
 const GENERIC_TITLE = CONTRACT_NAME; // Replace with what you want the generic titles to say if you want it to be different from the contract name.
-const GENERIC_DESCRIPTION = "What will be your Slogger?"; // Replace with what you want the generic descriptions to say.
+const GENERIC_DESCRIPTION = "What will be your Slogger"; // Replace with what you want the generic descriptions to say.
 const GENERIC_IMAGE = "https://ipfs.io/ipfs/bafybeie7s2upncdrfnkbx57u7hfqh6h7tutuapzmarad6vejekvbkkarz4"; // Replace with your generic image that will display for all NFTs pre-reveal.
 
 // Automatically set contract address if deployed using the deployContract.js script
